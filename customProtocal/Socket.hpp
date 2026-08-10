47afde44781570eb9763ae828124032550bee519
HeJie <2145381080@qq.com>
2026-08-10 21:43:32 +0800

    自定义协议封装

---
+++ customProtocal/Socket.hpp
@@ -0,0 +1,145 @@
+#pragma once
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include"log.hpp"
+#include"InetAddr.hpp"
+#include<memory>
+extern Log LOG;
+enum{
+    SUCCESS = 0,
+    SOCKET_INIT_ERROR,
+    BIND_ERROR,
+    LISTEN_ERROR
+};
+const static int gBackLog = 16;
+//关于tcp连接的三个关键接口
+//1.server端:listen accept
+//listen:将被bind的socket设置为监听状态,持续捕捉
+//accept:捕获监听转台的socket捕捉到的连接,并以套接字形式返回,这个socket就是与客户端通信的套接字
+//2.client端:connect
+//connect:相指定主机发送连接请求,三次握手成功后会该socket用于c/s通信
+class  Socket
+{
+
+protected:
+   Socket() 
+   {
+
+   }
+   //tcp
+   virtual std::shared_ptr<Socket>  netAccept(InetAddr& hostData) = 0;
+   virtual void createSocket() = 0;
+   virtual void netBind(InetAddr& hostData);
+   virtual bool netListen() = 0;
+   virtual int nettConnect() = 0;
+   virtual std::string netRecv() = 0;
+   virtual ssize_t netSendTo(std::string& message) = 0;
+   virtual bool netConnect(InetAddr& hostData) = 0;
+   virtual void resourceClose() = 0;
+   //udp
+
+protected:
+};
+
+class TcpSocket : public Socket
+{
+    public:
+    bool netConnect(InetAddr& hostData) override
+    {
+        int ret = connect(_socketFd,hostData.getSockAddrIn(),hostData.getLen());
+        if(ret == 0)
+            return true;
+        else
+            return false;
+    }
+    std::shared_ptr<Socket> netAccept(InetAddr& hostData) override
+    {
+        int condFd = accept(_socketFd,hostData.getSockAddrIn(),hostData.getLenAddress());
+        if(condFd ==-1)
+        {
+            LOG(WARN,"Accept error!\n");
+            return nullptr;
+        }
+        else
+        {
+            return std::make_shared<TcpSocket>(condFd);
+        } 
+
+    }
+    bool buildTcpSocket(InetAddr& hostData)
+    {
+        createSocket();
+        netBind(hostData);
+        netListen();
+    }
+    ssize_t netSendTo(std::string& message) override
+    {
+        ssize_t ret = send(_socketFd,message.c_str(),message.size(),0);
+        if(ret >= 0)
+        {
+            LOG(INFO,"Send success!\n");
+        }
+        else
+        {
+            LOG(WARN,"Send error!\n");
+        }
+    }
+
+    std::string netRecv() override
+    {
+        char buffer[1024];
+        ssize_t ret = recv(_socketFd,buffer,sizeof(buffer),0);
+        if(ret >= 0)
+        {
+             LOG(INFO,"Recv Success!\n");
+             return buffer;
+        }
+        else
+        {
+            LOG(WARN,"Recv error!\n");
+        }
+    }
+    void resourceClose() override
+    {
+        close(_socketFd);
+    }
+protected:
+    void createSocket() override
+    {
+       _socketFd = socket(AF_INET,SOCK_STREAM,0);
+       if(_socketFd < 0)
+       {
+            LOG(FATAL,"Socket init,error!\n");
+            exit(SOCKET_INIT_ERROR);
+       }
+
+    }
+    void netBind(InetAddr& hostData) override
+    {
+       int ret = bind(_socketFd,hostData.getSockAddrIn(),hostData.getLen());
+        if(bind < 0)
+        {
+            LOG(ERROR,"Bind error!\n");
+            exit(BIND_ERROR);
+        }
+    }
+   bool netListen() override
+   {
+       if(listen(_socketFd,gBackLog)!=0)
+       {
+            LOG(ERROR,"Listen error!\n");
+       }
+    
+   }
+private:
+   int _socketFd;
+
+};
+
+class UdpSocket
+{
+
+
+
+};
